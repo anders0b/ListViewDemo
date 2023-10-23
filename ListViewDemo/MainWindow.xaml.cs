a66509550a6c93453e68ebd8d0f0bb9065218baf
Anders Öberg <anders.oberg@live.se>
2023-10-23 14:11:21 +0200

    ny

--- ListViewDemo/MainWindow.xaml.cs
+++ ListViewDemo/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MovieDataAccess.Data;
 
 namespace ListViewDemo
 {
@@ -20,9 +22,51 @@ namespace ListViewDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DataSource DataSource { get; set; } = new DataSource();
+        public MainWindowContext MainWindowContext { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowContext = new MainWindowContext();
+            DataContext = MainWindowContext;
+            foreach (var product in DataSource.Stock)
+            {
+                Products.Items.Add(product);
+            }
+
+        }
+
+        private void Products_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Products.SelectedItem is Product selectedItem)
+            {
+                MainWindowContext.ProdName = selectedItem.Name;
+                MainWindowContext.ProdPrice = selectedItem.Price.ToString();
+            }
+        }
+
+        private void UpdateProdBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Products.SelectedItem is Product selectedItem)
+            {
+               //selectedItem.Name = NameText.Text;
+                //selectedItem.Price = double.Parse(PriceText.Text);
+
+                var selectedProduct = DataSource.Stock.FirstOrDefault(p => p.Name == selectedItem.Name);
+                if (selectedItem is null)
+                {
+                    return;
+                }
+                selectedProduct.Name = NameText.Text;
+                selectedProduct.Price = double.Parse(PriceText.Text);
+                Products.Items.Clear();
+                foreach (var product in DataSource.Stock)
+                {
+                    Products.Items.Add(product);
+                }
+
+            }
+
         }
     }
 }
---
+++ ListViewDemo/MainWindowContext.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ListViewDemo;
+
+public class MainWindowContext : INotifyPropertyChanged
+{
+    private string _prodPrice;
+
+    public string ProdPrice
+    {
+        get { return _prodPrice; }
+        set
+        {
+            _prodPrice = value; 
+            OnPropertyChanged();
+        }
+    }
+
+    private string _prodName;
+
+    public string ProdName
+    {
+        get { return _prodName; }
+        set
+        {
+            _prodName = value; 
+            OnPropertyChanged();
+        }
+    }
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
